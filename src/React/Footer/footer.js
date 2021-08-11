c54a52cc8eed52791076b6eccdcb7ba9f81026c1
ja54312 <ija54312@gmail.com>
2021-08-10 22:43:53 -0500

    Se modifico changelog a  v0.0.2

--- src/React/Footer/footer.js
+++ src/React/Footer/footer.js
@@ -6,7 +6,7 @@ export default function Footer(){
     return(
       <>
        <footer>
-       <span className="version"> v0.0.1</span>
+       <span className="version"> v0.0.2</span>
        <p class="text-footer">Created with love ❤ by <a href="https:www.ja54312.com" target="_blank" rel="noopener noreferrer" alt="enlace a pagina JA54312">JA54312</a></p>
            <div className="container-correo">
                <a href="mailto:marcosclass.edu@gmail.com">
