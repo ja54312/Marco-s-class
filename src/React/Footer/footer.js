a02458ebc3739f39572e9772a5c8c4637ab24144
ja54312 <ija54312@gmail.com>
2021-08-10 22:41:23 -0500

    metimos toda la informacion y dimos estilo basico al body

--- src/React/Footer/footer.js
+++ src/React/Footer/footer.js
@@ -9,7 +9,7 @@ export default function Footer(){
        <span className="version"> v0.0.1</span>
        <p class="text-footer">Created with love ❤ by <a href="https:www.ja54312.com" target="_blank" rel="noopener noreferrer" alt="enlace a pagina JA54312">JA54312</a></p>
            <div className="container-correo">
-               <a>
+               <a href="mailto:marcosclass.edu@gmail.com">
                    <img src="https://www.vectorlogo.zone/logos/gmail/gmail-icon.svg" alt="imagen correo"></img>
                </a>
            </div>
